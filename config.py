8aa3f8f290bc5608b7c56e9ef9d481206ef2d110
Alvinus Cardova <50035456+alvinus-cardova@users.noreply.github.com>
2025-02-23 05:15:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-DATA_PATH = 'flipkart_fashion_products_dataset.json'
+DATA_PATH = '/content/drive/MyDrive/flipkart-sales-forecasting/flipkart_fashion_products_dataset.json'
 SAVE_DIR = 'processed_data'
