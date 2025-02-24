12a70d7728819bd167de65119b245281658e794d
Alvinus Cardova <50035456+alvinus-cardova@users.noreply.github.com>
2025-02-24 16:34:35 +0700

    Delete features directory

--- features/engineering.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def create_features(df):
-    df['price_diff'] = df['actual_price'] - df['selling_price']
-    df['is_discounted'] = (df['discount_percentage'] > 0).astype(int)
-    return df
